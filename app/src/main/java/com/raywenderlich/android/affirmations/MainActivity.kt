02344b60152717be515c0342273a132bdceaabec
Ben Hagy <benhagy@alumni.vcu.edu>
2022-07-06 19:02:19 -0400

    first commit: all files in repository

---
+++ app/src/main/java/com/raywenderlich/android/affirmations/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.raywenderlich.android.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.raywenderlich.android.affirmations.adapter.ItemAdapter
+import com.raywenderlich.android.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myDataSet = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+        recyclerView.setHasFixedSize(true)
+
+    }
+}
---
+++ app/src/main/java/com/raywenderlich/android/affirmations/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.raywenderlich.android.affirmations.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceID: Int) {
+}
